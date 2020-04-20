e1de3b6f0222c8381a411ef2796b80ac7325629b
USER <user@USERnoMacBook-ea.local>
2020-04-20 10:34:47 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// if (false) {
+//     console.log('this is true');
+// } else if(true) {
+//     console.log('this is in else if block');  
+// } else {
+//     console.log('this is false');
+// }
+
+const num = 10;
+const bool = Boolean(num);
+console.log(num);
+// const num = Number(false);
+// console.log(num);
+
+// if (0 == '0' || === '0') {
+// if (0 == '0' && === '0') {
+// if (0 !== '1') {
+// if (0 != '1') { 
+// if (0 === '1') {
+// if (0 == '1') {
+// if(num) {
+if(!num) {
+    console.log('this is true');
+} else {
+    console.log('this is false');
+}
